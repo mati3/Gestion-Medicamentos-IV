a83cc35ee591d7c0907939b2169153c3923a96c6
Matilde Cabrera González <mati331@correo.ugr.es>
2018-10-13 21:53:50 +0200

    inicio sinatra app

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './sinatra/myapp' 
+
+run Sinatra::Application 
---
+++ sinatra/myapp.rb
@@ -0,0 +1,17 @@
+
+require 'sinatra' 
+
+  
+#  set :port, 8181
+
+  get '/' do
+    #erb :index
+    ' hola mundo '
+  end
+
+#  get // do
+#	pass if request.path_info == "/index"
+  	# ...
+#  end
+  
+
