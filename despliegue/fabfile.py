44c765e860cb8de66ec1a553f2e5d00106696042
Matilde Cabrera González <mati331@correo.ugr.es>
2019-01-13 00:26:43 +0100

    arreglo fabfile closed #34

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -2,37 +2,20 @@
 
 from fabric.api import *
 
-
-def Instalar():
-	""" Clonar repositorio """
-
-	run('git clone https://github.com/mati3/Gestion-Medicamentos-IV.git')
-	
-	run('sudo apt-get update')
-	run('sudo apt-get upgrade')
-	run('sudo apt install build-essential')
-	run('sudo apt install ruby')
-	run('sudo gem install sinatra')
-	run('sudo gem install bundle')
-	run('cd Gestion-Medicamentos-IV && bundle install')
-
-def Eliminar():
-	""" Eliminar el repositorio """
-
+def Remove():
 	run('sudo rm -rf ./Gestion-Medicamentos-IV')
+
+def Install():
+	Remove()
+	run('git clone https://github.com/mati3/Gestion-Medicamentos-IV.git')
+	run('cd Gestion-Medicamentos-IV && bundle install')
 	
-def Iniciar():
-	""" Run """
+def Run():
 	run('cd Gestion-Medicamentos-IV && sudo bundle exec rackup',pty=False)
-	
 
-"""
-#!/bin/bash
+def Update():
+	Install()
+	Run()
 
-vagrant up --provider=azure
 
-fab -f despliegue/fabfile.py -H vagrant@ivgestion-ip.cloudapp.azure.com Eliminar
-fab -f despliegue/fabfile.py -H vagrant@ivgestion-ip.cloudapp.azure.com Instalar
-fab -f despliegue/fabfile.py -H vagrant@ivgestion-ip.cloudapp.azure.com Iniciar
 
-"""
